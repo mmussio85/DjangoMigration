7197c5b07b471ccabff659d45a7988257b2dae70
Marcos Mussio <marcosmussio@Marcoss-MacBook-Pro.local>
2019-01-29 20:17:12 -0300

    Added unit test and about page.

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 #pages/urls.py
 from django.urls import path
 
-from .views import homePageView
-
+from .views import HomePageView
+from .views import AboutPageView
 
 urlpatterns = [
-	path('', homePageView, name='home')
+	path('', HomePageView.as_view(), name='home'),
+	path('about/', AboutPageView.as_view(), name='about') # new
 ]
 
